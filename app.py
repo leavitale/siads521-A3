39ef55d2db66a55dcb36f77b4a560e4d42fdce96
Lea Vitale <lea.vitale@outlook.fr>
2025-11-10 17:06:45 -0500

    update

--- app.py
+++ app.py
@@ -82,27 +82,26 @@ app.layout = html.Div([
     html.H1("Vehicle Emissions Dashboard", style={"textAlign": "center", "fontWeight":"bold"}),
 
     # I add the year slider
-    html.Label("Year Range"),
-    dcc.RangeSlider(id = "years", min = yr_min, max = yr_max, value = [yr_min, yr_max],
-                    step = 1, allowCross = False, marks = None,
-                    tooltip = {"placement":"bottom","always_visible":True}),
-
+    html.Div([html.Label("Year Range"),
+    dcc.RangeSlider(id = "years", min = yr_min, max = yr_max, value = [yr_min, yr_max],step = 1, allowCross = False, marks = None,
+                              tooltip = {"placement":"bottom","always_visible":True}),],
+             style = {"display":"inline-block", "width":"450px"}),
 
     # I add the brand widget for the treemap
-    html.Label("Treemap Brand"),
+    html.Div([html.Label("Treemap Brand"),
     dcc.Dropdown(id = "brand", options = car_emissions['MAKE'].drop_duplicates().sort_values(), value = default_brand,
-                 placeholder = "Select a Brand for the Treemap"),
+                 placeholder = "Select a Brand for the Treemap"),], style = {"display":"inline-block", "width":"300px"}),
 
     # I add a line break before my plots
     html.Br(),
 
     # I add the plots
 
-    html.Div([html.Div([dcc.Graph(id="line")], style={"width":"49%","display":"inline-block"}),
-    html.Div([dcc.Graph(id="treemap")], style={"width":"49%","display":"inline-block"}),
-    html.Div([dcc.Graph(id="bar")], style={"width":"49%","display":"inline-block"}),
-    html.Div([dcc.Graph(id="violin")], style={"width":"49%","display":"inline-block"}),
-    html.Div([dcc.Graph(id="scatter")], style={"width":"49%","display":"inline-block"}),]),
+        html.Div([html.Div([dcc.Graph(id="line")], style={"width":"49%","display":"inline-block"}),
+        html.Div([dcc.Graph(id="treemap")], style={"width":"49%","display":"inline-block"}),
+        html.Div([dcc.Graph(id="bar")], style={"width":"49%","display":"inline-block"}),
+        html.Div([dcc.Graph(id="violin")], style={"width":"49%","display":"inline-block"}),
+        html.Div([dcc.Graph(id="scatter")], style={"width":"49%","display":"inline-block"}),]),
 
 ])
 
